273095ae6af3e68ed17a1dd364477781127de203
MOUNCIF <Mons-L@users.noreply.github.com>
2021-03-28 16:04:45 +0200

    test

--- test.c
+++ test.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(){
 
-    printf("gmmg");
+int main(int argc, char *argv[]){
+    /*printf("%d\n",argc);
+    for(int i=0; i<argc; i++){
+        printf("arg%d=%s\n",i,argv[i]);
+    }*/
+
+    if(argc ==2){
+        if(argv[1][0] == '-' && argv[1][1] == 'i'){
+            //Gerer le mode interactif
+            printf("C'est le mode interactif\n");
+        }
+        else if(argv[1][0] == '-' && argv[1][1] == 'f'){
+            printf("C'est le mode batch\n");
+            //Recupere le chemin du fichier
+
+            //Faire le mode batch
+        }
+        else{
+            printf("Option non reconnue.\n");
+            printf("%s\n",argv[1]);
+        }
+    }
+    else{
+        printf("C'est le mode CLI\n");
+        //Faire la ligne de commande
+    }
     return 0;
 }
