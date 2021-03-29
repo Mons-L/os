cf9d86d941fede3b9756867dbe35a99814541263
S-BCH <S-BCH@users.noreply.github.com>
2021-03-29 17:59:16 +0200

    A

--- utilitaire.c
+++ utilitaire.c
@@ -1,3 +1,14 @@
+/*!
+ * \file utilitaire.c
+ * \brief Fichier d'implémentation des fonctions d'allocation et de désallocation de la mémoire.
+ * \author Salma BENCHELKHA - Mouncif LEKMITI - Farah MANOUBI
+ * \version 1.0
+ * \date 2 avril 2021
+ * \struct utilitaire.h
+ * 
+ * Ce fichier contient toutes les fonctions utiles à la manipulation de la mémoire.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/structure.h"
@@ -5,8 +16,14 @@
 #include "include/espaceMemoire.h"
 #include "include/utilitaire.h"
 
-
-/* initialisation de la zone de travail*/
+/*!
+ * \brief Fonction permettant l'initialisation de la mémoire de la zone de travail.
+ *        Elle permet d'allouer de la mémoire à l'aide du nombre de bytes passé en paramètre.
+ * 		  Elle permet également de mettre à nul le nombre de bytes alloue, la mémoire allouée et la mémoire libre.
+ * 		  Enfin, si l'adresse n'est pas égale à nul, elle effectue une insertion en tête en A REVOIR.
+ * \param [in,out] nbBytes Nombre de byte du bloc mémoire. Type int.  
+ * \return Un nombre de byte alloué.
+ */
 int initMemory(int nbBytes){
 	void* adresse = malloc(nbBytes);
 	int nbBytesAlloue = 0;
@@ -19,10 +36,15 @@ int initMemory(int nbBytes){
 	return nbBytesAlloue;
 }
 
-/* recuperation  de la zone initialement réservée*/
+/*!
+ * \brief Fonction permettant la récupération de la mémoire de la zone initialement réservée.
+ * \return Le nombre de byte récupéré.
+ */
 int freeMemory(){  
     int nbBytesRecupere = 0;
     Liste liste;
+	// Elle permet, tant que la mémoire allouée n'est pas égale à nulle, de libérer toutes
+	// les adresses qu'elles a utilisées et la liste en elles-mêmes.
     while(memoireAllouee != NULL){
         liste = memoireAllouee;
         memoireAllouee = memoireAllouee->suivant;
@@ -30,6 +52,8 @@ int freeMemory(){
         free(liste);
         liste = NULL;
     }
+	// Elle permet, tant que la mémoire libre n'est pas égale à nulle, de récupérer tout 
+	// les bytes utilisés dans le bloc mémoire et de libérer la liste.
     while(memoireLibre != NULL){
         liste = memoireLibre;
         memoireLibre = memoireLibre->suivant;
@@ -37,23 +61,35 @@ int freeMemory(){
         free(liste);
         liste = NULL;
     }
+	// Elle permet, si le nombre de byte récupéré est égal à 0, de le placer à -1 en cas d'erreur.
+	// Par exemple, si la mémoire a déja été libérée.
     if(nbBytesRecupere == 0)
         nbBytesRecupere = -1;
     return nbBytesRecupere;
 }
 
-/*allocation dynamique d'espace dans la zone*/
+/*!
+ * \brief Fonction permettant l'allocation dynamique d'espace dans la mémoire libre.
+ * \param [in] nbBytes Nombre de byte du bloc mémoire. Type int.  
+ * \return L'adresse allouée.
+ */
 void* myalloc(int nbBytes){
 	void* addresseAllouee = NULL;
 	Liste liste = NULL;
+	// Si le nombre de bytes est supérieur à 0, elle effectue une recherche d'élément dans la mémoire libre.
     if(nbBytes > 0)
         liste = rechercheElement(memoireLibre,nbBytes);
+	// Si la liste n'est pas égal à nulle et si le nombre de bytes d'un bloc mémoire de la mémoire libre
+	// est égale au nombre de bytes passé en paramètre, l'adresse de ce bloc mémoire devient l'adresse allouée.
+	// La mémoire libre est ensuite attribué à la mémoire allouée.
 	if(liste != NULL){
 		if(liste->blocMemoire.nbBytes == nbBytes){
 			addresseAllouee = liste->blocMemoire.adresse;
 			printf("Avant insert : \n");
 			Liste z = memoireLibre;
 			printf("Memoire libre { ");
+			// Tant quand la mémoire allouée n'est pas égale à nulle, 
+			// on écrit le nombre de byte de chaque bloc mémoire de la mémoire allouée.
 			while(z != NULL){
 				printf("%d",z->blocMemoire.nbBytes);
 				z = z->suivant;
@@ -99,10 +135,18 @@ void* myalloc(int nbBytes){
     return addresseAllouee;
 }
 
- /*désallocation d'une zone adressée par un pointeur */
+/*!
+ * \brief Fonction permettant la désallocation dynamique d'espace dans une zone adressée par un pointeur.
+ * \param [in] p pointeur vers l'adresse mémoire recherchée. Type void*.
+ * \return Un nombre de byte.
+ */
  int myfree(void* p){
     int nbBytes = -1;
+	// On recherche un bloc mémoire dans la mémoire allouée avec le pointeur passé en paramètre.
 	Liste liste = rechercheBlocMemoire(memoireAllouee, p);
+	// Si le pointeur vers l'adresse mémoire recherchée et le bloc mémoire trouvé ne sont pas égal à nul,
+	// on effectue une insertion en tête de la mémoire libre du bloc mémoire trouvé précédemment.
+	// On supprime ensuite ce bloc mémoire de la mémoire allouée.
 	if(p != NULL && liste != NULL ){
 		nbBytes = liste->blocMemoire.nbBytes;
 		memoireLibre = inserTete(memoireLibre,nbBytes,liste->blocMemoire.adresse);
@@ -111,6 +155,11 @@ void* myalloc(int nbBytes){
     return nbBytes;
  }
 
+/*!
+ * \brief Fonction permettant l'affichage des messages concernant l'initialisation de la mémoire.
+ * \param [in] nbBytesAlloue Nombre de byte alloué du bloc mémoire. Type int. 
+ * \param [in] nbBytes Nombre de byte du bloc mémoire. Type int.   
+ */
  void initMemoryMessage(int nbBytesAlloue,int nbBytes){
 	 if(nbBytesAlloue == 0)     
 	 	printf("Initialisation de la zone de travail echouee (%d bytes)\n",nbBytes);
@@ -118,6 +167,10 @@ void* myalloc(int nbBytes){
 		printf("Initialisation de la zone de travail reussie (%d bytes)\n",nbBytes);
 }
 
+/*!
+ * \brief Fonction permettant l'affichage des messages concernant la récuperation de la mémoire.
+ * \param [in] nbBytesRecupere Nombre de byte récupéré du bloc mémoire. Type int. 
+ */
 void freeMemoryMessage(int nbBytesRecupere){
 	if(nbBytesRecupere == -1)
 		printf("Recuperation de la zone memoire initialisee pour le programme echouee\n");
@@ -125,6 +178,11 @@ void freeMemoryMessage(int nbBytesRecupere){
 		printf("Recuperation de la zone memoire initialisee pour le programme reussie %d bytes recupere\n",nbBytesRecupere);
 }
 
+/*!
+ * \brief Fonction permettant l'affichage des messages concernant l'allocation de mémoire.
+ * \param [in] p pointeur vers l'adresse mémoire recherchée. Type void*.
+ * \param [in] nbBytes Nombre de byte du bloc mémoire. Type int. 
+ */
 void myallocMessage(void* p,int nbBytes){
 	if(p != NULL){
 		printf("Allocation de memoire dans la zone de travail reussi (%d bytes)\n",nbBytes);
@@ -134,6 +192,10 @@ void myallocMessage(void* p,int nbBytes){
 		printf("Allocation de memoire dans la zone de travail echouee (%d bytes)\n",nbBytes);
 }
 
+/*!
+ * \brief Fonction permettant l'affichage des messages concernant la récupération de mémoire.
+ * \param [in] nbBytesRecupere Nombre de byte récupéré du bloc mémoire. Type int. 
+ */
 void myfreeMessage(int nbBytesRecupere){
 	if(nbBytesRecupere == -1)
 		printf("Desallocation de memoire dans la zone de travail echouee\n");
